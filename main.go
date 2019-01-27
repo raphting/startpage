7205531c3417c9e20fc977554a14c16f743bfad5
Raphael <developer@raphaelsprenger.de>
2019-01-27 22:49:59 +0100

    periodically rewrite front page

--- main.go
+++ main.go
@@ -15,20 +15,38 @@ import (
 )
 
 var globalPage string
+var APIKey string
 
 func main() {
-	APIKey := os.Getenv("STARTPAGE_API_KEY")
+	APIKey = os.Getenv("STARTPAGE_API_KEY")
 	if APIKey == "" {
 		fmt.Println("The environment variable STARTPAGE_API_KEY (Unsplash API access key) is required.")
 		return
 	}
 
+	generateFront()
+	go runLoop()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		uname, url, _ := callUnsplash(APIKey)
-		parseFrontToGlobal(uname, url)
 		fmt.Fprintf(w, globalPage)
 	})
 	log.Fatal(http.ListenAndServe(":9691", nil))
+}
+
+func runLoop() {
+	ticker := time.NewTicker(5 * time.Minute)
+	for {
+		select {
+		case <-ticker.C:
+			generateFront()
+		}
+	}
+}
+
+func generateFront() {
+	fmt.Println("Generate and publish new front page")
+	uname, url, _ := callUnsplash(APIKey)
+	parseFrontToGlobal(uname, url)
 }
 
 func parseFrontToGlobal(username, imageurl string) error {
